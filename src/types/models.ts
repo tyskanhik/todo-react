0d4bf1f0b90ae46a2c638a8cef1e30d0e401fad8
Arctic Fox <tyskanhik@mail.ru>
2025-06-13 21:19:02 +0300

    create ui-kit Text

---
+++ src/types/models.ts
@@ -0,0 +1,10 @@
+export interface Todo {
+  id: string;
+  text: string;
+  completed: boolean;
+}
+
+export interface TodoState {
+  todos: Todo[];
+  filter: 'all' | 'active' | 'completed'
+}
---
+++ src/ui-kit/Text.tsx
@@ -0,0 +1,69 @@
+import React from "react";
+import styles from './Text.module.scss';
+
+import classNames from "classnames";
+
+
+type TextVariant = 
+  | 'h1' | 'h2' | 'h3' | 'h4' | 'h5' | 'h6'  // Заголовки
+  | 'body' | 'caption';          // Текстовые варианты
+
+type TextWeight = 
+  | 'thin'       // 100
+  | 'light'      // 300
+  | 'regular'    // 400
+  | 'medium'     // 500
+  | 'bold'       // 700
+  | 'black';     // 900
+
+interface TextProps {
+  variant?: TextVariant;
+  weight?: TextWeight;
+  color?: string;
+  size?: number | string;
+  align?: 'left' | 'center' | 'right';
+  style?: React.CSSProperties;
+  children: React.ReactNode;
+  onClick?: () => void;
+}
+
+export const Text: React.FC<TextProps> = ({
+  variant = 'body',
+  weight = 'regular',
+  color,
+  size,
+  align = 'center',
+  style,
+  children,
+  onClick
+}) => {
+  const Tag = variant.startsWith('h') 
+    ? variant : variant === 'body' 
+      ? 'p' : 'span';
+
+  const textStyle = {
+    ...(color && { color }),
+    ...(size && { fontSize: typeof size === 'number' 
+      ? `${size}px` : size 
+    }),
+    ...style
+  }
+
+  const textClasses = classNames(
+    styles.text,
+    styles[`text--${variant}`],
+    styles[`text--${weight}`],
+    styles[`text--align${align}`]
+  )
+  
+
+  return (
+    <Tag 
+      style={textStyle}
+      className={textClasses}
+      onClick={onClick}
+    >
+      { children }
+    </Tag>
+  )
+}
