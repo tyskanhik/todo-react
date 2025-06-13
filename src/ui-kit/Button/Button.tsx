14d4a8832c92897a1ecc8138eb729f4eb453abd2
Arctic Fox <tyskanhik@mail.ru>
2025-06-13 22:08:08 +0300

    create ui-kit button

---
+++ src/ui-kit/Button/Button.tsx
@@ -0,0 +1,90 @@
+import React from "react";
+import styles from './Button.module.scss';
+import classNames from "classnames";
+
+type ButtonColor = 
+  | 'primary' 
+  | 'secondary' 
+  | 'success' 
+  | 'danger' 
+  | 'warning'
+  | 'custom';
+
+type ButtonCustomColor = {
+  color: string;
+  backgroundColor: string;
+}
+
+interface ButtonProps {
+  text: string;
+  width?: number | string;
+  height?: number | string;
+  color?: ButtonColor;
+  backgroundColor?: string;
+  customColor?: ButtonCustomColor; // Для color='custom'
+  disabled?: boolean;
+  borderRadius?: number | string;
+  fontSize?: string | number;
+  icon?: React.ReactNode;
+  iconPosition?: 'left' | 'right';
+  gap?: number | string;
+  style?: React.CSSProperties;
+  onClick?: () => void;
+}
+
+export const Button: React.FC<ButtonProps> = ({
+  text,
+  width = 'auto',
+  height = '44px',
+  color = 'primary',
+  backgroundColor,
+  customColor,
+  disabled = false,
+  borderRadius = '22px',
+  fontSize = '16px',
+  icon,
+  iconPosition,
+  gap = '5px',
+  style,
+  onClick
+}) => {
+  const buttonStyle = {
+    width: typeof width === 'number' 
+      ? `${width}px` : width,
+    height: typeof height === 'number' 
+      ? `${height}px` : height,
+    ...(color && { color: color }),
+    ...(backgroundColor && { backgroundColor: backgroundColor}),
+    ...(color === 'custom' && customColor && { 
+      color: customColor.color,
+      backgroundColor: customColor.backgroundColor
+    }),
+    borderRadius: typeof borderRadius === 'number' 
+      ? `${borderRadius}px` : borderRadius,
+    fontSize: typeof fontSize === 'number' 
+      ? `${fontSize}px` : fontSize,
+    gap: typeof gap === 'number' ? `${gap}px` : gap,
+    ...style
+  }
+
+  const buttonClasses = classNames(
+    styles.button,
+    styles[`button--${color}`],
+    disabled ? styles['button--disabled'] : ''
+  )
+
+  return (
+    <button 
+      style={buttonStyle}
+      className={buttonClasses}
+      >
+      {icon && iconPosition === 'left' && (
+        <span className={styles.iconLeft}>{icon}</span>
+      )}
+      { text }
+      {icon && iconPosition === 'right' && (
+        <span className={styles.iconRight}>{icon}</span>
+      )}
+    </button>
+  )
+}
